1af4d40446e26b2a4b8a98ed04136f2da884cff1
Davi Melo <davicosta01082008@gmail.com>
2024-09-02 20:49:01 -0300

    fix: make results singular when their value is 1

--- src/components/result-display.tsx
+++ src/components/result-display.tsx
@@ -8,15 +8,18 @@ export function ResultDisplay({ result }: ResultDisplayProps) {
   return (
     <div>
       <p className="text-2xl font-extrabold italic tracking-tight text-off-black sm:text-3xl">
-        <span className="text-purple">{result?.years ?? '- -'}</span> years
+        <span className="text-purple">{result?.years ?? '- -'}</span>{' '}
+        {result?.years === 1 ? 'year' : 'years'}
       </p>
 
       <p className="text-2xl font-extrabold italic tracking-tight text-off-black sm:text-3xl">
-        <span className="text-purple">{result?.months ?? '- -'}</span> months
+        <span className="text-purple">{result?.months ?? '- -'}</span>{' '}
+        {result?.months === 1 ? 'month' : 'months'}
       </p>
 
       <p className="text-2xl font-extrabold italic tracking-tight text-off-black sm:text-3xl">
-        <span className="text-purple">{result?.days ?? '- -'}</span> days
+        <span className="text-purple">{result?.days ?? '- -'}</span>{' '}
+        {result?.days === 1 ? 'day' : 'days'}
       </p>
     </div>
   )
